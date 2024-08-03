2a3a154938117a927b5f2b48faed737f486795c2
anjor <anjor@umd.edu>
2024-08-03 09:20:05 +0100

    add date

--- tooth-fairy-rates.py
+++ tooth-fairy-rates.py
@@ -1,9 +1,10 @@
-from enum import Enum
+from datetime import datetime
 from fasthtml.common import *
 
 
 def render(tf_rate):
     return Tr(
+        Td(tf_rate.created_at),
         Td(tf_rate.location),
         Td(tf_rate.rate),
     )
@@ -14,6 +15,7 @@ app, rt, tf_rates, TF_Rate = fast_app(
     live=True,
     render=render,
     id=int,
+    created_at=datetime,
     location=str,
     rate=float,
     pk="id",
@@ -22,6 +24,9 @@ app, rt, tf_rates, TF_Rate = fast_app(
 
 @rt("/")
 def get():
+
+    sorted_rates = sorted(tf_rates(), key=lambda x: x.location)
+
     return Titled(
         "Tooth Fairy Rates",
         Form(
@@ -32,7 +37,8 @@ def get():
             method="post",
         ),
         Table(
-            *tf_rates(),
+            Tr(Th(B("Date")), Th(B("Location")), Th(B("Rate"))),
+            *sorted_rates,
         ),
     )
 
@@ -45,7 +51,11 @@ class TFRate:
 
 @rt("/add_rate")
 def post(rate: TFRate):
-    r = TF_Rate(location=rate.location, rate=rate.rate)
+    r = TF_Rate(
+        created_at=datetime.today().strftime("%Y-%m-%d"),
+        location=rate.location,
+        rate=rate.rate,
+    )
     tf_rates.insert(r)
     return RedirectResponse("/", status_code=303)
 
